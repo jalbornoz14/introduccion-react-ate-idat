61feb2960ce499624e6ea7e170a3d13f8cdba9a3
Jhefferson <jhefferson.albornoz@gdpteam.com>
2025-08-16 09:07:48 -0500

    fetch get

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -3,17 +3,19 @@ import { CardPokemon } from "../components/CardPokemon.jsx";
 
 export function Pokemons() {
   const [pokemons, setPokemons] = useState([]);
-  const [pokemon, setPokemon] = useState(null)
+  const [pokemon, setPokemon] = useState(null);
 
-  const onSearchPokemon = async (url) =>{
+  const onSearchPokemon = async (url) => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => setPokemon(data))
       .catch((error) => console.error(error));
-  }
+  };
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    fetch("https://pokeapi.co/api/v2/pokemon", {
+      method: "GET",
+    })
       .then((response) => response.json())
       .then((data) => setPokemons(data.results))
       .catch((error) => console.error(error));
@@ -23,13 +25,22 @@ export function Pokemons() {
     <>
       <h1>Listar pokemones</h1>
       <div>
-        {
-          pokemon ? <div> <img src={pokemon.sprites.front_default} alt="" /></div> : <></>
-        }
+        {pokemon ? (
+          <div>
+            {" "}
+            <img src={pokemon.sprites.front_default} alt="" />
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
       <div className="list-pokemon">
         {pokemons.map((e) => (
-          <CardPokemon name={e.name} url={e.url} onSearchPokemon={onSearchPokemon} />
+          <CardPokemon
+            name={e.name}
+            url={e.url}
+            onSearchPokemon={onSearchPokemon}
+          />
         ))}
       </div>
     </>
